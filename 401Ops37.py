425d45f938dc8410bc5a604b2378c50f00561eae
jmcano50 <jcano@aol.com>
2024-02-27 11:01:56 -0800

    401Ops37.py

--- 401Ops37.py
+++ 401Ops37.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# Script Name:                  Ops Challenge: Cookie Capture Capades
+# Author:                       Juan Miguel Cano 
+# Date of latest revision:      02/27/2024
+# Resource:                     https://chat.openai.com/share/baf7c736-8491-42a5-9a66-4440e38e54d8
+# Purpose:                      The pupose of this script is to demonstrate web scraping techniques and cookie management.
+
 
 import requests
 
